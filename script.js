9d0567c36ba9b172f3109841b5b4ae2793dc598d
Maximilian Mitterrutzner <maximitterrutzner@gmail.com>
2025-04-19 22:21:32 +0200

    Improve and simplify logic structure

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // abcdefghijklmnopqrstuvwxyz
-const identified = new Map();
-const words = []; // TODO maybe just store cipher and calculate clear whenever needed? Then a set can be used here
+const substitutions = Array(letters.length).fill("?");
+let words = new Set();
 
 function init() {
     registerCiphertextFormHandler();
-    fillLetterContainer();
+    update();
 }
 
 function registerCiphertextFormHandler() {
@@ -12,59 +12,57 @@ function registerCiphertextFormHandler() {
     ciphertextForm.addEventListener('submit', function (event) {
         event.preventDefault();
 
-        const formData = new FormData(this);
-        const ciphertext = formData.get("txtCiphertext");
-        console.log(ciphertext);
-        ciphertext.split(" ").forEach((word) => {
-            if (word.trim() !== ""
-                && words.find(([cipher, _]) => cipher === word) === undefined) {
-                words.push([word, cipherToClear(word)]);
-            }
-        })
+        const added = new Set(new FormData(this).get("txtCiphertext").toUpperCase().split(" ").map(w => w.trim()).filter(Boolean));
+        words = words.union(added);
         ciphertextForm.reset();
-        updateWordTable();
+        update();
     });
+}
+
+function update() {
+    updateLetterContainer();
+    updateWordTable();
 }
 
 function cipherToClear(cipher) {
     let clear = "";
-    for (const char in cipher) {
-        clear += identified.get(char) || ".";
+    for (const char of cipher) {
+        clear += substitutions[letters.indexOf(char)];
     }
     return clear;
 }
 
 function updateWordTable() {
-    console.log(words);
     const wordTableBody = document.getElementById("wordTableBody");
-    wordTableBody.innerHTML = ""
+    wordTableBody.innerHTML = "";
 
-    words.forEach(([cipher, plain]) => {
+    words.forEach(word => {
         const row = document.createElement("tr");
         const removeCell = document.createElement("td");
         removeCell.textContent = "X";
         const cipherCell = document.createElement("td");
-        cipherCell.textContent = cipher;
+        cipherCell.textContent = word;
         const plainCell = document.createElement("td");
-        plainCell.textContent = plain;
+        plainCell.textContent = cipherToClear(word);
         row.append(removeCell, cipherCell, plainCell)
         wordTableBody.appendChild(row);
     });
 }
 
-function fillLetterContainer() {
+function updateLetterContainer() {
     const letterContainer = document.getElementById("letterContainer");
+    letterContainer.innerHTML = "";
 
     for (var i = 0; i < letters.length; i++) {
-        letterContainer.appendChild(createLetterBox(letters[i]));
+        letterContainer.appendChild(createLetterBox(letters[i], substitutions[i]));
     }
 }
 
-function createLetterBox(letter) {
+function createLetterBox(letter, identified) {
     const letterBox = document.createElement("label");
     letterBox.className = "letterbox";
     const content = document.createElement("div");
-    content.textContent = "?";
+    content.textContent = identified;
     letterBox.append(letter, content);
     return letterBox;
 }
